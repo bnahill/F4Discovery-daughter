33a51afb32e447b5b4e1042ad9b26e5c7290f2c0
Alexandre Courtemanche <alex.courtemanche1@gmail.com>
2013-03-26 15:30:17 -0400

    Added the gates and pin configurations.

--- cpld/src/f4_daughter.vhd
+++ cpld/src/f4_daughter.vhd
@@ -40,15 +40,26 @@ port(
 	EXTI8	: out std_logic;
 	EXTI15	: out std_logic;
 	EXTI11	: out std_logic;
+	EXTI12	: out std_logic;
 	EXTI7	: out std_logic;
 	EXTI5	: out std_logic;
 	EXTI4	: out std_logic;
 	EXTI3	: out std_logic;
+	EXTI2	: out std_logic;
 
 	-- Power Settings
 	POW_MODE	: out std_logic;
 	POW_HPWR	: out std_logic;
-	POW_NXT	: out std_logic
+	POW_NXT		: out std_logic;
+	
+	-- Touch Screen 
+	FSMC_NEx	: in std_logic;
+	FSMC_NWE	: in std_logic;
+	BKLT_EN		: out std_logic;
+	nCS			: out std_logic;
+	nRD			: out std_logic;
+	nWR			: out std_logic
+	
 );
 
 end entity;
@@ -56,6 +67,28 @@ end entity;
 architecture RTL of f4_daughter is
 
 begin
+
 	LED1 <= S1 and S2;
 	LED2 <= S3 and S4;
+	
+	-- Accelerometer interrupt
+	EXTI11 <= INT1_ACC;
+	EXTI15 <= INT1_GYRO;
+	EXTI13 <= INT2_ACC;
+	EXTI12 <= INT2_GYRO;
+	
+	-- Individual interrupts
+	EXTI3 <= RF_IRQ;
+	EXTI2 <= TOUCH_IRQ;
+	
+	-- Button Interrupts
+	EXTI5 <= S1;
+	EXTI6 <= S2;
+	EXTI7 <= S3;
+	EXTI8 <= S4;
+	
+	nRD <= not FSMC_NWE;
+	nWR <= FSMC_NWE; 
+	nCS <= FSMC_NEx;
+	
 end RTL;
